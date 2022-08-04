942d968bb3f785a2924271305caf005325a614e4
jvitoralb <jvvalb@gmail.com>
2022-08-04 20:16:38 -0300

    creating model and schema

--- myApp.js
+++ myApp.js
@@ -3,7 +3,17 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+let personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+let Person = mongoose.model('Person', personSchema);
+console.log(Person, personSchema)
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
