bdc35a2f4b6d53f1901b2d5c905f5aa8f8e12342
jvitoralb <jvvalb@gmail.com>
2022-08-05 22:10:19 -0300

    fix update

--- myApp.js
+++ myApp.js
@@ -64,7 +64,7 @@ const findPersonById = (personId, done) => {
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
   Person.findByIdAndUpdate(personId,
-    {favoriteFoods: this.favoriteFoods.push(foodToAdd)},
+    {favoriteFoods: this.PersonModel.favoriteFoods.push(foodToAdd)},
     (err, data) => {
       if (err) return console.log(err);
 
