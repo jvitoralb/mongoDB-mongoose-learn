3ad0da0fecb7b5ab0a24fbdaa7572cb96a28ac66
jvitoralb <jvvalb@gmail.com>
2022-08-06 11:25:37 -0300

    fixing bug

--- myApp.js
+++ myApp.js
@@ -92,9 +92,9 @@ const removeById = (personId, done) => {
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  Person.remove({name: nameToRemove}, (err, data) => {
+  Person.remove({name: nameToRemove}, (err, response) => {
     if (err) return console.log(err);
-    done(null, data);
+    done(null, response);
   });
 };
 
